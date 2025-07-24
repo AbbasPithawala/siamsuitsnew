797360dcd2fba6f37180a65511403ad837754007
abbasPithawala <abbaspithawala098@gmail.com>
2025-07-24 14:52:59 +0530

    just a commitdss

--- siamServer/tailor/auth.js
+++ siamServer/tailor/auth.js
@@ -16,8 +16,6 @@ const auth = async(req, res, next) => {
             data: {}
         })
     }
-    // hello
-    // console.log(tailor)
     req.tailor = tailor
     req.token = token 
     next()
